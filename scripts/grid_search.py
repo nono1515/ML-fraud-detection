c6dea63627979b5800b9d56700cc4cad4b5b55c8
Noé Duruz <noe.duruz@gmail.com>
2024-02-26 00:05:51 +0100

    added features_to_keep

--- scripts/grid_search.py
+++ scripts/grid_search.py
@@ -4,24 +4,49 @@ import itertools
 import subprocess
 
 # Automated grid search experiments
-eta_list = [0.1, 0.3]
+eta_list = [0.1, 0.3, 0.5]
 max_depth_list = [4, 6]
-# subsample_list = [0.5, 1]
-alpha_list = [0, 1]
+subsample_list = [0.5, 1]
+alpha_list = [0]
+
+features_to_keep_list = [
+    [],  # keep all
+    [
+        "Class",
+        "V4",
+        "V14",
+        "V10",
+        "V12",
+        "Amount",
+        "V18",
+        "V7",
+        "V17",
+        "V16",
+        "V19",
+        "V15",
+        "V26",
+    ],
+]
 
 # Iterate over all combinations of hyperparameter values.
-for eta, max_depth, alpha in itertools.product(eta_list, max_depth_list, alpha_list):
+for features_to_keep, eta, max_depth, subsample, alpha in itertools.product(
+    features_to_keep_list, eta_list, max_depth_list, subsample_list, alpha_list
+):
     subprocess.run(
         [
             "dvc",
             "exp",
             "run",
             "dvc.yaml",
-            # "--queue",
+            "--queue",
+            "--set-param",
+            f"params.yaml:prepare.features_to_keep={features_to_keep}",
             "--set-param",
             f"params.yaml:train.xgb_params.eta={eta}",
             "--set-param",
             f"params.yaml:train.xgb_params.max_depth={max_depth}",
+            "--set-param",
+            f"params.yaml:train.xgb_params.subsample={subsample}",
             "--set-param",
             f"params.yaml:train.xgb_params.alpha={alpha}",
         ],
